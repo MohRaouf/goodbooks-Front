cfb18897c7c08725bcebdc048fea4ef756af850d
MohRaouf <moh.proff@gmail.com>
2021-04-17 20:20:28 +0200

    Test netlify

--- src/app/components/admin-books/admin-books.component.ts
+++ src/app/components/admin-books/admin-books.component.ts
@@ -5,6 +5,7 @@ import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap'
 import { AdminService } from 'src/app/services/admin.service';
 import { Book, Category, Author } from '../../models/admin-models'
 import { convertToBase64 } from '../../helpers/image-helpers'
+import { PublicService } from 'src/app/services/public.service';
 
 @Component({
   selector: 'app-admin-books',
@@ -13,7 +14,7 @@ import { convertToBase64 } from '../../helpers/image-helpers'
 })
 export class AdminBooksComponent implements OnInit, OnDestroy {
 
-  constructor(private modalService: NgbModal, private adminService: AdminService, private router: Router) { }
+  constructor(private modalService: NgbModal, private adminService: AdminService, private router: Router, private publicService: PublicService) { }
 
   subscriber: any
   books: Array<Book> = []
@@ -49,6 +50,17 @@ export class AdminBooksComponent implements OnInit, OnDestroy {
     })
   }
 
+  /** Search for Books */
+  keyWords: string = "";
+  Search(e: any) {
+    this.loading=true;
+    this.publicService.getBookSearchRes(this.keyWords).subscribe((response: any) => {
+      this.books = response.body
+      this.loading=false;
+    })
+  }
+
+  /**pagination */
   showPageIndex(pageIndex: any) {
     this.loading = true;
     this.page = pageIndex;
@@ -72,7 +84,7 @@ export class AdminBooksComponent implements OnInit, OnDestroy {
 
     /* Incase of updating book - populate its data in the modal */
     caller.name != "add" && this.bookForm.patchValue({ name: book.name, description: book.description });
-    this.modalService.open(content, { centered: true, animation: true }).result.then((result) => {
+    this.modalService.open(content, { centered: true }).result.then((result) => {
 
       /* Check for the caller either update or insert and execute its method */
       caller.name == "add" ? this.insertBook() : this.updateBook(book);
@@ -80,7 +92,7 @@ export class AdminBooksComponent implements OnInit, OnDestroy {
       /** clear modal content */
       this.bookForm.controls.name.setValue('')
       this.bookForm.controls.description.setValue('')
-      this.img=''
+      this.img = ''
 
       this.closeResult = `Closed with: ${content}`;
     }, (reason) => {
@@ -88,7 +100,7 @@ export class AdminBooksComponent implements OnInit, OnDestroy {
       /** clear modal content */
       this.bookForm.controls.name.setValue('')
       this.bookForm.controls.description.setValue('')
-      this.img=''
+      this.img = ''
 
       console.log(this.closeResult)
     });
--- src/app/services/public.service.ts
+++ src/app/services/public.service.ts
@@ -6,55 +6,55 @@ import { Subject } from 'rxjs/Subject';
   providedIn: 'root'
 })
 export class PublicService {
-  searchCategory:string=""
-  searchedName:string=""
+  searchCategory: string = ""
+  searchedName: string = ""
 
   searchCatChange: Subject<string> = new Subject<string>();
   searchedNameChange: Subject<string> = new Subject<string>();
-  categoryObservable=this.searchCatChange.asObservable()
+  categoryObservable = this.searchCatChange.asObservable()
   searchedNameObservable = this.searchedNameChange.asObservable()
-  constructor(private client: HttpClient) { 
- 
-  }
-  updateSearch(NewCat:string,NewName:string){
+
+  constructor(private client: HttpClient) { }
+  
+  updateSearch(NewCat: string, NewName: string) {
     this.searchCatChange.next(NewCat)
     this.searchedNameChange.next(NewName)
   }
-  getAllBooks(page:number,perPage:number) {
+  getAllBooks(page: number, perPage: number) {
     return this.client.get(`${config.apiUrl}/books?page=${page}&perPage=${perPage}`, { observe: 'response' })
   }
-  getTopBooks(){
-    return this.client.get(`${config.apiUrl}/books/top`,{observe:"response"});
+  getTopBooks() {
+    return this.client.get(`${config.apiUrl}/books/top`, { observe: "response" });
   }
-  getBookById(id:number){
+  getBookById(id: number) {
     return this.client.get(`${config.apiUrl}/books/${id}`)
   }
-  getBookSearchRes(search:string){
-    return this.client.get(`${config.apiUrl}/books/search/${search}`,{observe:"response"})
+  getBookSearchRes(search: string) {
+    return this.client.get(`${config.apiUrl}/books/search/${search}`, { observe: "response" })
   }
-  getAllAuthors(page:any=undefined,perPage:any=undefined){
-    return this.client.get(`${config.apiUrl}/authors?page=${page}&perPage=${perPage}`,{ observe: 'response' }) 
+  getAllAuthors(page: any = undefined, perPage: any = undefined) {
+    return this.client.get(`${config.apiUrl}/authors?page=${page}&perPage=${perPage}`, { observe: 'response' })
   }
-  getTopAuthors(){
-    return this.client.get(`${config.apiUrl}/authors/top`,{observe:"response"});
+  getTopAuthors() {
+    return this.client.get(`${config.apiUrl}/authors/top`, { observe: "response" });
   }
-  getAuthorById(id:number){
+  getAuthorById(id: number) {
     return this.client.get(`${config.apiUrl}/authors/${id}`)
   }
-  getAuthorSearchRes(search:string){
-    return this.client.get(`${config.apiUrl}/authors/search/${search}`,{observe:"response"})
+  getAuthorSearchRes(search: string) {
+    return this.client.get(`${config.apiUrl}/authors/search/${search}`, { observe: "response" })
   }
-  getAllCategories(page:any=undefined,perPage:any=undefined){
-    return this.client.get(`${config.apiUrl}/categories?page=${page}&perPage=${perPage}`,{observe:"response"})
+  getAllCategories(page: any = undefined, perPage: any = undefined) {
+    return this.client.get(`${config.apiUrl}/categories?page=${page}&perPage=${perPage}`, { observe: "response" })
   }
-  getTopCategories(){
-    return this.client.get(`${config.apiUrl}/categories/top`,{observe:"response"});
+  getTopCategories() {
+    return this.client.get(`${config.apiUrl}/categories/top`, { observe: "response" });
   }
-  getCategoryById(id:number){
+  getCategoryById(id: number) {
     return this.client.get(`${config.apiUrl}/categories/${id}`)
   }
-  getCatSearchRes(search:string){
-    return this.client.get(`${config.apiUrl}/categories/search/${search}`,{observe:"response"})
+  getCatSearchRes(search: string) {
+    return this.client.get(`${config.apiUrl}/categories/search/${search}`, { observe: "response" })
   }
 }
 /*getBooks(page:number,count:number) {
