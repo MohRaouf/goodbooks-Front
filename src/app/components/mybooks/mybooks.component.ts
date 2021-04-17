9d76c6c98bd6663d2ae46910768266d797060ecb
rjOwl <mostafayossef100@yahoo.com>
2021-04-17 09:23:13 +0200

    Added mybooks component; parent component for shelf-menu and shelf-table

---
+++ src/app/components/mybooks/mybooks.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuService } from 'src/app/services/menu.service';
+import { loggedUserServices } from 'src/app/services/loggedUser.service';
+
+
+@Component({
+  selector: 'app-mybooks',
+  templateUrl: './mybooks.component.html',
+  styleUrls: ['./mybooks.component.css']
+})
+
+export class MybooksComponent implements OnInit {
+  shelf = [];
+  menu:any
+
+  constructor(
+    private userShelfSerivce: loggedUserServices,
+    private menuSerivce: MenuService) { }
+
+  ngOnInit(): void {
+    this.shelf = this.userShelfSerivce.userShelf;
+    this.menu = this.menuSerivce.options;
+
+    this.userShelfSerivce.getBookSehlf("a")
+    .subscribe((data:any)=>{
+      console.log(data)
+      if(data.status == 200){
+        this.shelf = data.body.result
+        this.userShelfSerivce.userShelf = data.body.result
+      // console.log(this.shelf)
+    }
+    },(err)=>{
+      console.log(err)
+    })
+  }
+
+  loadShelf(e:any){
+    console.log(e)
+    this.userShelfSerivce.getBookSehlf(e.target.id)
+    .subscribe((data:any)=>{
+      this.shelf = data.body
+      console.log(this.shelf)
+    },(err)=>{
+      console.log(err)
+    })
+  }
+
+
+}
